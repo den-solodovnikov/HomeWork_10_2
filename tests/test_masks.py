a8d923eba4c0ae4d4a03f000c353d36fdab57a63
Den Sol <densfmost@mail.ru>
2026-03-02 11:22:40 +0300

    Commit add test_masks

---
+++ tests/test_masks.py
@@ -0,0 +1,27 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+@pytest.mark.parametrize("card_number, expected",
+                         [("7000792289606361", "7000 79** **** 6361"),
+                          ("1234123412341234", "1234 12** **** 1234"),
+                          ("1234  1234 1234 1234", "1234 12** **** 1234"),
+                          ("123412341234123456", "1234 12** **** 3456"),
+                          ("123412341234", "1234 12** **** 1234"),
+                          ("", "")
+                          ])
+def test_get_mask_card_number(card_number: str, expected: str):
+    assert get_mask_card_number(card_number) == expected
+
+def test_get_mask_card_number_type_error():
+    with pytest.raises(TypeError):
+        test_get_mask_card_number_type_error("4562kisj.64")
+
+@pytest.mark.parametrize("account_number, expected",
+                         [("73654108430135874305", "**4305"),
+                          ("1234  1234 1234 1234", "**1234"),
+                          ("123412341234123456", "**3456"),
+                          ("123", "**123"),
+                          ("", "")
+                          ])
+def test_get_mask_account(account_number: str, expected: str):
+    assert get_mask_account(account_number) == expected
