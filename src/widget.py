0c3629cd9905f32ee299f66de44f1e9ff638ea6c
Den Sol <densfmost@mail.ru>
2026-03-02 12:37:09 +0300

    Commit add test_widget

--- src/widget.py
+++ src/widget.py
@@ -11,17 +11,18 @@ def mask_account_card(number_account: str) -> str:
     if 'Счет' in number_account:
         number_account_masked = (
                 number_account.replace(new_number_account, "") +
-                get_mask_account(int(new_number_account))
+                get_mask_account(new_number_account)
                                  )
     else:
         number_account_masked = (
                 number_account.replace(new_number_account, "")
-                + get_mask_card_number(int(new_number_account))
+                + get_mask_card_number(new_number_account)
         )
     return number_account_masked
 
 
 def get_date(date_str: str) -> str:
     """ Возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
-    new_date = '.'.join(reversed(date_str[0:10].split('-')))
+    # new_date = '.'.join(reversed(date_str[0:10].split('-')))
+    new_date = re.sub(r"(\d+).(\d+).(\d+)",r"\3.\2.\1", date_str)
     return new_date
