bff89d2202a705fd081e2a5bc641075f8096b1d0
Den Sol <densfmost@mail.ru>
2026-03-16 12:39:34 +0300

    Commit add generators.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from src.widget import get_date
 from src.widget import mask_account_card
 from src.processing import filter_by_state
 from src.processing import sort_by_date
+from src.generators import (filter_by_currency, transaction_descriptions,
+                            card_number_generator)
 
 __name__ = "main"
 
@@ -24,3 +26,47 @@ print(sort_by_date([
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
 ], False))
+
+transactions = [{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+          "id": 142264268,
+          "state": "EXECUTED",
+          "date": "2019-04-04T23:20:05.206878",
+          "operationAmount": {
+              "amount": "79114.93",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+                  }
+              },
+          "description": "Перевод со счета на счет",
+          "from": "Счет 19708645243227258542",
+          "to": "Счет 75651667383060284188"
+       }]
+
+for card_number in card_number_generator(1, 100):
+    print(card_number)
+
+usd_transactions = filter_by_currency(transactions, "USD")
+for _ in range(2):
+    print('Djn')
+    print(next(usd_transactions))
+
+descriptions = transaction_descriptions(transactions)
+for _ in range(2):
+    print(next(descriptions))
+
