a2f807915aa3c1f407bced4085dcf6950d6f3f88
Den Sol <densfmost@mail.ru>
2026-04-15 21:52:08 +0300

    Commit add logging unit masks

--- main.py
+++ main.py
@@ -14,81 +14,81 @@ from src.utils import data_transactions
 
 __name__ = "main"
 
-print(get_mask_card_number('7000792289606361'))  # 7000 79** **** 6361
-print(get_mask_account('73654108430135874305'))  # **4305
-print(mask_account_card('Счет 64686473678894779589'))
-print(get_date("2024-03-11T02:26:18.671407"))
-print(filter_by_state([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ], 'CANCELED'
-))
-print(sort_by_date([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-], False))
-
-transactions = [{
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "USD",
-                  "code": "USD"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      },
-      {
-          "id": 142264268,
-          "state": "EXECUTED",
-          "date": "2019-04-04T23:20:05.206878",
-          "operationAmount": {
-              "amount": "79114.93",
-              "currency": {
-                  "name": "USD",
-                  "code": "USD"
-                  }
-              },
-          "description": "Перевод со счета на счет",
-          "from": "Счет 19708645243227258542",
-          "to": "Счет 75651667383060284188"
-       }]
-
-for card_number in card_number_generator(1, 100):
-    print(card_number)
-
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print('Djn')
-    print(next(usd_transactions))
-
-descriptions = transaction_descriptions(transactions)
-for _ in range(2):
-    print(next(descriptions))
-
-print(amount_transactions(
-    {
-        "id": 41428829,
-        "state": "EXECUTED",
-        "date": "2019-07-03T18:35:29.512364",
-        "operationAmount": {
-            "amount": "8221.37",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
-        "description": "Перевод организации",
-        "from": "MasterCard 7158300734726758",
-        "to": "Счет 35383033474447895560"
-    }
-))
+print(get_mask_account(''))  # **4305
+print(get_mask_card_number('87985555231345658624'))  # 7000 79** **** 6361
+# print(mask_account_card('Счет 64686473678894779589'))
+# print(get_date("2024-03-11T02:26:18.671407"))
+# print(filter_by_state([
+#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+#     ], 'CANCELED'
+# ))
+# print(sort_by_date([
+#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+# ], False))
+#
+# transactions = [{
+#           "id": 939719570,
+#           "state": "EXECUTED",
+#           "date": "2018-06-30T02:08:58.425572",
+#           "operationAmount": {
+#               "amount": "9824.07",
+#               "currency": {
+#                   "name": "USD",
+#                   "code": "USD"
+#               }
+#           },
+#           "description": "Перевод организации",
+#           "from": "Счет 75106830613657916952",
+#           "to": "Счет 11776614605963066702"
+#       },
+#       {
+#           "id": 142264268,
+#           "state": "EXECUTED",
+#           "date": "2019-04-04T23:20:05.206878",
+#           "operationAmount": {
+#               "amount": "79114.93",
+#               "currency": {
+#                   "name": "USD",
+#                   "code": "USD"
+#                   }
+#               },
+#           "description": "Перевод со счета на счет",
+#           "from": "Счет 19708645243227258542",
+#           "to": "Счет 75651667383060284188"
+#        }]
+#
+# for card_number in card_number_generator(1, 100):
+#     print(card_number)
+#
+# usd_transactions = filter_by_currency(transactions, "USD")
+# for _ in range(2):
+#     print('Djn')
+#     print(next(usd_transactions))
+#
+# descriptions = transaction_descriptions(transactions)
+# for _ in range(2):
+#     print(next(descriptions))
+#
+# print(amount_transactions(
+#     {
+#         "id": 41428829,
+#         "state": "EXECUTED",
+#         "date": "2019-07-03T18:35:29.512364",
+#         "operationAmount": {
+#             "amount": "8221.37",
+#             "currency": {
+#                 "name": "USD",
+#                 "code": "USD"
+#             }
+#         },
+#         "description": "Перевод организации",
+#         "from": "MasterCard 7158300734726758",
+#         "to": "Счет 35383033474447895560"
+#     }
+# ))
