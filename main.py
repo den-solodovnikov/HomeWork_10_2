f992bedf1a9a9c99df208e6fd4efae766a8cdb88
Den Sol <densfmost@mail.ru>
2026-04-15 22:42:46 +0300

    Commit add logging unit utils

--- main.py
+++ main.py
@@ -75,20 +75,21 @@ print(get_mask_card_number('87985555231345658624'))  # 7000 79** **** 6361
 # for _ in range(2):
 #     print(next(descriptions))
 #
-# print(amount_transactions(
-#     {
-#         "id": 41428829,
-#         "state": "EXECUTED",
-#         "date": "2019-07-03T18:35:29.512364",
-#         "operationAmount": {
-#             "amount": "8221.37",
-#             "currency": {
-#                 "name": "USD",
-#                 "code": "USD"
-#             }
-#         },
-#         "description": "Перевод организации",
-#         "from": "MasterCard 7158300734726758",
-#         "to": "Счет 35383033474447895560"
-#     }
-# ))
+print(amount_transactions(
+    {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+            "amount": "8221.37",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+    }
+))
+print(data_transactions('data/operations.json'))
--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,50 @@
 import json
+import logging
 
 from src.external_api import currency_conversion
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8", mode="w")
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def data_transactions(path_json: str) -> list[dict]:
     """ Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список. """
     try:
+        logger.info(f'Выполняется попытка открытия JSON-файла: {path_json}')
         with open(path_json, "r", encoding="utf-8") as file_transactions:
             transactions = json.load(file_transactions)
+            logger.info(f'Файл: {path_json} успешно открыт')
             if transactions:
+                logger.info('Операция считывания данных завершена успешно')
                 return transactions
             else:
+                logger.error(f'Файл {path_json} не содержит данных')
                 return []
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        logger.error(f'Ошибка открытия файла {path_json}: {ex}')
         return []
-    except json.decoder.JSONDecodeError:
+    except json.decoder.JSONDecodeError as ex:
+        logger.error(f'Ошибка декодировки файла {path_json}: {ex}')
         return []
 
 
 def amount_transactions(transaction: dict) -> float:
     """ Функция принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях.
-     Усли транзакция в была USD или в EUR, обращается к внешнему API
-     для получения текущего курса валют и конвертации суммы операции в рубли."""
+     Если транзакция была в USD или в EUR, обращается к внешнему API
+     для получения текущего курса валют и конвертации суммы операции в рубли. """
     currency_code = transaction['operationAmount']['currency']['code']
     amount = 0
     if currency_code == 'RUB':
+        logger.info('Сумма получена успешно, транзакция в RUB')
         amount = float(transaction['operationAmount']['amount'])
     elif currency_code == 'USD' or currency_code == 'EUR':
+        logger.info('Сумма получена успешно конвертацией в RUB')
         amount = currency_conversion(currency_code, transaction['operationAmount']['amount'])
     return amount
 
